f9a61cbfdcac4a4ae6177139ab16af66238e0a9c
Ken Moore <ken@ixsystems.com>
2019-01-23 10:15:34 -0500

    Disable the pages site from the lumina-website sub-repo. Publishing on the main lumina-desktop.github.io repo now, this repo is just for the hugo sources and publish script.

--- publish_hugo.sh
+++ publish_hugo.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #Script to take the statically-generated pages and deploy them with nginx
 DIST_DIR="$1"
-FINAL_SITE="lumina-desktop.github.io/lumina-website/"
+FINAL_SITE="lumina-desktop.github.io"
 
 basedir=`dirname $0`
 basedir=`realpath -q "${basedir}"`
 
 if [ -z "${DIST_DIR}" ] ; then
-  DIST_DIR="${basedir}/docs"
+  echo "No website directory specified! (argument 1 on command line)."
+  echo "This should be pointing to the local clone of the lumina-website.github.io repository (live site sources)."
+  return 1
 fi
 
 # Build the static site with Hugo and deploy it in the dist dir
