5713782c7fe496cbf09f131f4a1d3da8a8d04a6a
Ken Moore <moorekou@gmail.com>
2021-06-28 13:42:02 -0400

    Adjust the publish_hugo.sh script to build/deploy to the "docs" directory.

--- publish_hugo.sh
+++ publish_hugo.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 #Script to take the statically-generated pages and deploy them with nginx
-DIST_DIR="$1"
 FINAL_SITE="lumina-desktop.github.io"
 
 basedir=`dirname $0`
 basedir=`realpath -q "${basedir}"`
 
-if [ -z "${DIST_DIR}" ] ; then
-  echo "No website directory specified! (argument 1 on command line)."
-  echo "This should be pointing to the local clone of the lumina-website.github.io repository (live site sources)."
-  return 1
-fi
-
 # Build the static site with Hugo and deploy it in the dist dir
-hugo --ignoreCache -b "https://${FINAL_SITE}" -s "${basedir}/hugo-site" -d "${DIST_DIR}" --cleanDestinationDir
+hugo --ignoreCache -b "https://${FINAL_SITE}" -s "${basedir}/hugo-site" -d "${basedir}/docs" --cleanDestinationDir
+if [ $? -eq 0 ] ; then
+  git add "${basedir}/docs"
+  echo "------------"
+  echo "Site Rebuilt: commit and push the changes to GitHub to make it live"
+fi
 exit $?
